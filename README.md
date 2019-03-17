55458261b28dc6acfcdcdf55887a0974fddb7934
unknown <defria@gmail.com>
2019-03-17 16:00:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# NativeScript-Vue-WebView
+
+> A native application built with NativeScript-Vue 
+- This is a NativeScript-Vue example
+- This is an extension of [alexziskind1](https://github.com/alexziskind1) from his demo [NativeScript WebView URL Load Detection](https://www.youtube.com/watch?v=e5IxqIazEuY&t=207s). 
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Build for production
+tns build <platform> --bundle
+
+# Build, watch for changes and debug the application
+tns debug <platform> --bundle
+
+# Build, watch for changes and run the application
+tns run <platform> --bundle
+```
---
+++ app/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Page class="page" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <GridLayout columns="*" rows="*">
+      <WebView ref="myWebView" :src="url" v-on:loaded="onPageLoaded()"/>
+    </GridLayout>
+  </Page>
+</template>
+
+<script >
+export default {
+  components: {},
+
+  data() {
+    return {
+      url: "https://nativescripting.com/"
+    };
+  },
+  mounted() {},
+  methods: {
+    onPageLoaded() {
+      let webview = this.$refs.myWebView._nativeView;
+      webview.on("loadStarted", args => {console.log("LOAD STARTED EVENT", args.url)});
+      webview.on("loadFinished", args => {console.log("LOAD FINISHED EVENT", args.url)});
+    }
+  }
+};
+</script>
+<style>
+</style>
